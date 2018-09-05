9b11612d8c8aa8be4f33931ce6de374763cd9e15
tomtomato <tomzacchia@live.com>
2018-09-05 16:59:48 -0400

    Added functionality to dynamically add ingredients. Need to set up controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,27 @@ import css from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient' 
 
 const burger = (props) => {
+    // transforming object to array
+    // Object.keys return an array that only contains the keys of props, i.e 'salad'
+    // Array(n) returns empty array of size n
+    let ingredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_,i) => {
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+            })
+        })
+        .reduce((arr, el)=> {
+            // flatening array via concat, merging
+            return arr.concat(el);
+        }, []);
+    if (ingredients.length === 0){
+        ingredients = <p> Please start adding ingredients! </p>
+    }
+    console.log(ingredients);
     return (
         <div className={css.burger}>
             <BurgerIngredient type="breadTop"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="breadBottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            // salad: 0,
+            // bacon: 0,
+            // cheese: 0,
+            // meat: 0
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render(){
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div> Build Controls </div>
             </Aux>
         );
